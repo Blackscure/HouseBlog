b7946af11502c92a8992ca1183fe92c10ab25cb7
Blackscure <wekesabuyahi@gmail.com>
2021-06-21 23:26:14 +0300

    nav links styled set to fles and hover

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -6,20 +6,20 @@
          </div>
          <div class="nav-links">
              <ul>
-                <router-links class="link" to='#'>Home</router-links>
-                <router-links class="link" to='#'>Blogs</router-links>
-                <router-links class="link" to='#'>Create Post</router-links>
-                <router-links class="link" to='#'>Login/Register</router-links>
+                <router-link class="link" to='#'>Home</router-link>
+                <router-link class="link" to='#'>Blogs</router-link>
+                <router-link class="link" to='#'>Create Post</router-link>
+                <router-link class="link" to='#'>Login/Register</router-link>
              </ul>
          </div>
     </nav>
     <menuIcon/>
     <transition name="mobile-nav">
            <ul>
-                <router-links class="link" to='#'>Home</router-links>
-                <router-links class="link" to='#'>Blogs</router-links>
-                <router-links class="link" to='#'>Create Post</router-links>
-                <router-links class="link" to='#'>Login/Register</router-links>
+                <router-link class="link" to='#'>Home</router-link>
+                <router-link class="link" to='#'>Blogs</router-link>
+                <router-link class="link" to='#'>Create Post</router-link>
+                <router-link class="link" to='#'>Login/Register</router-link>
              </ul>
     </transition>
 </header>
@@ -36,5 +36,39 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+header {
+    background-color: #fff;
+    padding: 0 25px;
+    box-shadow: 5px 10px #888888;
+    z-index: 99;
+}
+
+.link {
+    font-weight: 500;
+    padding: 0 8px;
+    transition: .3s color ease;
+
+    &:hover {
+        color: aqua;
+    }
+}
+
+nav {
+    display: flex;
+    padding: 25px 0;
+
+    .branding {
+        display: flex;
+        align-items: center;
+
+        .header {
+            font-weight: 600;
+            font-size: 24px;
+            color: black;
+            text-decoration: none;
+        }
+    }
+}
 
 </style>
