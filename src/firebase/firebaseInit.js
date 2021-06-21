1e59c0d7a15c7a82a946f31aa197b6b5e09ebbf8
Blackscure <wekesabuyahi@gmail.com>
2021-06-21 21:37:23 +0300

    firebase set ad and initialized

---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCyOTSqiP04_Ajyivn_GTP02BoJ_Plyq2c",
+    authDomain: "blog-55674.firebaseapp.com",
+    projectId: "blog-55674",
+    storageBucket: "blog-55674.appspot.com",
+    messagingSenderId: "598413487302",
+    appId: "1:598413487302:web:fae060e29b7763d5083bc7"
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+
+  export { timestamp };
+  export default firebaseApp.firebase();
