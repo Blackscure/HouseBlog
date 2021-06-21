c03a48b58eddddc9187fd7083692b41e7a09ed66
Blackscure <wekesabuyahi@gmail.com>
2021-06-21 22:34:03 +0300

    mobile tannsition added in navigation file

---
+++ src/components/Navigation.vue
@@ -0,0 +1,40 @@
+<template>
+<header>
+    <nav class="conntainer">
+         <div class="branding">
+             <router-link class="header" :to="{ name: 'Home' }">HouseBlogs</router-link>
+         </div>
+         <div class="nav-links">
+             <ul>
+                <router-links class="link" to='#'>Home</router-links>
+                <router-links class="link" to='#'>Blogs</router-links>
+                <router-links class="link" to='#'>Create Post</router-links>
+                <router-links class="link" to='#'>Login/Register</router-links>
+             </ul>
+         </div>
+    </nav>
+    <menuIcon/>
+    <transition name="mobile-nav">
+           <ul>
+                <router-links class="link" to='#'>Home</router-links>
+                <router-links class="link" to='#'>Blogs</router-links>
+                <router-links class="link" to='#'>Create Post</router-links>
+                <router-links class="link" to='#'>Login/Register</router-links>
+             </ul>
+    </transition>
+</header>
+</template>
+
+<script>
+import menuIcon from '../assets/Icons/bars-regular.svg';
+export default {
+    name: 'navigation',
+    components:{
+        menuIcon
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
